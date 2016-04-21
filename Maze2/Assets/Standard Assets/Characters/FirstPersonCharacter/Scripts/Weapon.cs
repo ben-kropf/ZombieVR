df6f8bbbcf65735d4b0ccc9f05781bd53cf592ed
Austin Nwachukwu <anwachu1@umd.edu>
2016-04-20 23:14:31 -0400

    First Person Player
    Added First Person Character controls with scripts and audio (under
    Standard Assets, may move to parent Assets folder later)

---
+++ Maze2/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+﻿/* Weapon.cs
+ *
+ * Component of FirstPersonCharacter
+ * Spawns projectile from camera
+ *
+ * Author: Austin Nwachukwu
+ */
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (AudioSource))]
+public class Weapon : MonoBehaviour {
+
+	public float bulletSpeed = 50;
+	public Rigidbody bullet;
+	private AudioSource m_AudioSource;
+	public AudioClip m_FireSound;
+
+	private void Start() {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+	private void PlayFireSound() {
+        m_AudioSource.clip = m_FireSound;
+        m_AudioSource.Play();
+    }
+
+	void Fire() {
+		Rigidbody bulletClone = (Rigidbody) Instantiate(bullet, transform.position, transform.rotation);
+		bulletClone.velocity = transform.forward * bulletSpeed;
+	}
+	
+	void FixedUpdate () {
+        if (Input.GetMouseButtonDown(0)) {
+        	Fire();
+        	PlayFireSound();
+        }
+    }
+
+}
