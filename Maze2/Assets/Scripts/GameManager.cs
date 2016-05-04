c27da17337bba47e7bfbd66b8246e470abe11e39
VincentAlonso <vincent.j.alonso@gmail.com>
2016-05-03 23:46:58 -0400

    Fixed
    Lots of bugs

--- Maze2/Assets/Scripts/GameManager.cs
+++ Maze2/Assets/Scripts/GameManager.cs
@@ -12,25 +12,25 @@ public class GameManager : MonoBehaviour {
 	private Player playerInstance;
 
 	private void Start () {
-		StartCoroutine(BeginGame());
+		BeginGame();
 	}
 	
 	private void Update () {
-		if (Input.GetKeyDown(KeyCode.Enter)) {
+		if (Input.GetKeyDown(KeyCode.Return)) {
 			RestartGame();
 		}
 	}
 
-	private IEnumerator BeginGame () {
+	private void BeginGame () {
 		Camera.main.clearFlags = CameraClearFlags.Skybox;
 		Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
 		mazeInstance = Instantiate(mazePrefab) as Maze;
 		mazeInstance.Generate();
 		mazeInstance.transform.localScale = new Vector3(3.0f,3.0f,3.0f);
-		playerInstance = Instantiate(playerPrefab) as Player;
-		playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
-		Camera.main.clearFlags = CameraClearFlags.Depth;
-		Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
+		//playerInstance = Instantiate(playerPrefab) as Player;
+		//playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
+		//Camera.main.clearFlags = CameraClearFlags.Depth;
+		//Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
 
 	}
 
@@ -40,6 +40,6 @@ public class GameManager : MonoBehaviour {
 		if (playerInstance != null) {
 			Destroy(playerInstance.gameObject);
 		}
-		StartCoroutine(BeginGame());
+		BeginGame();
 	}
 }
